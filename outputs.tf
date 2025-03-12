0623f0e1c7c809312deac8bfd9b3da2fc75d17b8
Abraão Silva <abraaojs.dev@gmail.com>
2025-03-12 13:30:28 -0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -4,16 +4,16 @@ output "vpc_id" {
 }
 
 output "private_subnets" {
-  description = "Lista de subnets privadas (com região e ID)"
-  value       = { for k, v in google_compute_subnetwork.private : k => { region = v.region, id = v.id } }
+  description = "IDs das sub-redes privadas"
+  value       = { for k, v in google_compute_subnetwork.private : k => v.id }
 }
 
 output "public_subnets" {
-  description = "Lista de subnets públicas (com região e ID)"
-  value       = { for k, v in google_compute_subnetwork.public : k => { region = v.region, id = v.id } }
+  description = "IDs das sub-redes públicas"
+  value       = { for k, v in google_compute_subnetwork.public : k => v.id }
 }
 
 output "database_subnets" {
-  description = "Lista de subnets de banco de dados (com região e ID)"
-  value       = { for k, v in google_compute_subnetwork.database : k => { region = v.region, id = v.id } }
+  description = "IDs das sub-redes de banco de dados"
+  value       = { for k, v in google_compute_subnetwork.database : k => v.id }
 }
