4c7ac2dc7e3ccf32db86be85b09970bfc5410bb0
Abraão Silva <abraaojs.dev@gmail.com>
2025-03-12 13:19:14 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,31 +1,37 @@
+# ID do projeto GCP
 variable "project_id" {
   description = "ID do projeto no GCP"
   type        = string
 }
 
+# Região onde os recursos serão criados
 variable "region" {
   description = "Região do GCP onde os recursos serão criados"
   type        = string
   default     = "us-central1"
 }
 
+# Nome da VPC
 variable "vpc_name" {
   description = "Nome da VPC"
   type        = string
   default     = "devsecops-vpc"
 }
 
-variable "private_subnets_cidrs" {
-  description = "Lista de CIDRs para sub-redes privadas"
-  type        = list(string)
+# Mapa de subnets privadas (nome + CIDR)
+variable "private_subnets" {
+  description = "Mapa de sub-redes privadas (Nome e CIDR)"
+  type        = map(string)
 }
 
-variable "public_subnets_cidrs" {
-  description = "Lista de CIDRs para sub-redes públicas"
-  type        = list(string)
+# Mapa de subnets públicas (nome + CIDR)
+variable "public_subnets" {
+  description = "Mapa de sub-redes públicas (Nome e CIDR)"
+  type        = map(string)
 }
 
-variable "database_subnets_cidrs" {
-  description = "Lista de CIDRs para sub-redes de banco de dados"
-  type        = list(string)
+# Mapa de subnets para banco de dados (nome + CIDR)
+variable "database_subnets" {
+  description = "Mapa de sub-redes de banco de dados (Nome e CIDR)"
+  type        = map(string)
 }
