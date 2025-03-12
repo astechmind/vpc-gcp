4a11a7797d799b702f3874a1b0e1990486b08ac8
Abraão Silva <abraaojs.dev@gmail.com>
2025-03-12 13:25:09 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,37 +1,52 @@
-# ID do projeto GCP
+# ID do projeto GCP onde os recursos serão criados
 variable "project_id" {
   description = "ID do projeto no GCP"
   type        = string
 }
 
-# Região onde os recursos serão criados
+# Região principal onde a VPC será criada
 variable "region" {
   description = "Região do GCP onde os recursos serão criados"
   type        = string
   default     = "us-central1"
 }
 
+# Lista de regiões secundárias (para Multi-AZ)
+variable "secondary_regions" {
+  description = "Lista de regiões adicionais para distribuir as subnets (Multi-AZ)"
+  type        = list(string)
+  default     = ["us-east1", "us-west1"]  # Exemplo de 3 regiões
+}
+
 # Nome da VPC
 variable "vpc_name" {
   description = "Nome da VPC"
   type        = string
-  default     = "devsecops-vpc"
 }
 
-# Mapa de subnets privadas (nome + CIDR)
+# Subnets privadas distribuídas em múltiplas regiões
 variable "private_subnets" {
-  description = "Mapa de sub-redes privadas (Nome e CIDR)"
-  type        = map(string)
+  description = "Mapa de subnets privadas com nome, região e CIDR"
+  type = map(object({
+    cidr   = string
+    region = string
+  }))
 }
 
-# Mapa de subnets públicas (nome + CIDR)
+# Subnets públicas distribuídas em múltiplas regiões
 variable "public_subnets" {
-  description = "Mapa de sub-redes públicas (Nome e CIDR)"
-  type        = map(string)
+  description = "Mapa de subnets públicas com nome, região e CIDR"
+  type = map(object({
+    cidr   = string
+    region = string
+  }))
 }
 
-# Mapa de subnets para banco de dados (nome + CIDR)
+# Subnets para banco de dados distribuídas em múltiplas regiões
 variable "database_subnets" {
-  description = "Mapa de sub-redes de banco de dados (Nome e CIDR)"
-  type        = map(string)
+  description = "Mapa de subnets de banco de dados com nome, região e CIDR"
+  type = map(object({
+    cidr   = string
+    region = string
+  }))
 }
