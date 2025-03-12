a779784a98627147fe49b16649832963589193c9
Abraão Silva <abraaojs.dev@gmail.com>
2025-03-12 13:25:58 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,39 +3,39 @@ resource "google_compute_network" "vpc" {
   name                    = var.vpc_name
   project                 = var.project_id
   auto_create_subnetworks = false
-  routing_mode            = "REGIONAL"
+  routing_mode            = "GLOBAL"  # Ativado para suportar Multi-AZ
 }
 
-# Subnets Privadas (com nomes e IP Privado do Google ativado)
+# Subnets Privadas (Multi-AZ)
 resource "google_compute_subnetwork" "private" {
   for_each = var.private_subnets
 
   name                     = each.key
-  ip_cidr_range            = each.value
-  region                   = var.region
+  ip_cidr_range            = each.value.cidr
+  region                   = each.value.region
   project                  = var.project_id
   network                  = google_compute_network.vpc.id
   private_ip_google_access = true
 }
 
-# Subnets Públicas
+# Subnets Públicas (Multi-AZ)
 resource "google_compute_subnetwork" "public" {
   for_each = var.public_subnets
 
   name          = each.key
-  ip_cidr_range = each.value
-  region        = var.region
+  ip_cidr_range = each.value.cidr
+  region        = each.value.region
   project       = var.project_id
   network       = google_compute_network.vpc.id
 }
 
-# Subnets para Banco de Dados
+# Subnets para Banco de Dados (Multi-AZ)
 resource "google_compute_subnetwork" "database" {
   for_each = var.database_subnets
 
   name          = each.key
-  ip_cidr_range = each.value
-  region        = var.region
+  ip_cidr_range = each.value.cidr
+  region        = each.value.region
   project       = var.project_id
   network       = google_compute_network.vpc.id
 }
