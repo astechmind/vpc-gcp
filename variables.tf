0b99ddc6105f19b412d310479a3708d0bf7644ce
Abraão Silva <abraaojs.dev@gmail.com>
2025-03-13 19:06:56 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,36 +1,42 @@
-# ID do projeto GCP onde os recursos serão criados
 variable "project_id" {
   description = "ID do projeto no GCP"
   type        = string
 }
 
-# Região do GCP onde a VPC será criada
 variable "region" {
   description = "Região do GCP onde os recursos serão criados"
   type        = string
   default     = "us-central1"
 }
 
-# Nome da VPC
 variable "vpc_name" {
   description = "Nome da VPC"
   type        = string
 }
 
-# Subnets privadas (com nomes e CIDRs)
 variable "private_subnets" {
   description = "Mapa de sub-redes privadas com nome e CIDR"
-  type        = map(string)
+  type        = map(object({
+    name  = string
+    cidr  = string
+    region = string
+  }))
 }
 
-# Subnets públicas (com nomes e CIDRs)
 variable "public_subnets" {
   description = "Mapa de sub-redes públicas com nome e CIDR"
-  type        = map(string)
+  type        = map(object({
+    name  = string
+    cidr  = string
+    region = string
+  }))
 }
 
-# Subnets para banco de dados (com nomes e CIDRs)
 variable "database_subnets" {
   description = "Mapa de sub-redes de banco de dados com nome e CIDR"
-  type        = map(string)
+  type        = map(object({
+    name  = string
+    cidr  = string
+    region = string
+  }))
 }
